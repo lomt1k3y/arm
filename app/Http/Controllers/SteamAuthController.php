202935a1a8e183be24aea3a1af7b4b19dc3e7c4e
lomt1k3y <yakushin_1985@bk.ru>
2024-12-16 16:54:57 +0300

    +
    добавил отработку ошибок, редирект на админку, аватар и имя изменяются при каждой новой авторизации если в стим профиле что-то менялось

--- app/Http/Controllers/SteamAuthController.php
+++ app/Http/Controllers/SteamAuthController.php
@@ -16,6 +16,72 @@ use Ilzrv\LaravelSteamAuth\Exceptions\Validation\ValidationException;
 use Ilzrv\LaravelSteamAuth\SteamAuthenticator;
 use Ilzrv\LaravelSteamAuth\SteamUserDto;
 
+// final class SteamAuthController
+// {
+//     public function __invoke(
+//         Request $request,
+//         Redirector $redirector,
+//         Client $client,
+//         HttpFactory $httpFactory,
+//         AuthManager $authManager,
+//     ): RedirectResponse {
+//         $steamAuthenticator = new SteamAuthenticator(
+//             new Uri($request->getUri()),
+//             $client,
+//             $httpFactory,
+//         );
+
+//         try {
+//             $steamAuthenticator->auth();
+//         } catch (ValidationException | SteamResponseNotValidAuthenticationException) {
+//             return $redirector->to(
+//                 $steamAuthenticator->buildAuthUrl()
+//             ); 
+//         }
+//         $steamUser = $steamAuthenticator->getSteamUser();
+//         session(['username' => $steamUser->getPersonaName()]); 
+
+//         $authManager->login(
+//             $this->firstOrCreate($steamUser),
+//             true
+//         );
+//         session()->flash('success', 'Вы успешно авторизованы через Steam!');
+//         return $redirector->to('/'); // Переход на страницу /home
+//     }
+
+//     private function firstOrCreate(SteamUserDto $steamUser): User
+//     {
+
+//         // Пытаемся найти пользователя по steam_id
+//         $user = User::where('steam_id', $steamUser->getSteamId())->first();
+
+//         // Если пользователь найден, проверяем, изменилось ли имя
+//         if ($user) {
+//             // Если имя изменилось, обновляем его
+//             if ($user->name !== $steamUser->getPersonaName()) {
+//                 $user->name = $steamUser->getPersonaName();
+//                 $user->save(); // Сохраняем изменения в базе данных
+//             }
+//             if ($user->avatar !== $steamUser->getAvatarFull()) {
+//                 $user->avatar = $steamUser->getAvatarFull();
+//                 $user->save(); // Сохраняем изменения в базе данных
+//             }
+//                return $user;
+        
+//             }
+//         // Если пользователь не найден, создаем нового
+//         return User::create([
+//             'steam_id' => $steamUser->getSteamId(),
+//             'name' => $steamUser->getPersonaName(), // Убедитесь, что эта функция возвращает корректное значение.
+//             'avatar' => $steamUser->getAvatarFull(),
+//             'player_level' => $steamUser->getPlayerLevel(),
+//         ]);
+//     }
+// }
+
+///ОТРАБОТКА ОШИБОК
+
+
 final class SteamAuthController
 {
     public function __invoke(
@@ -30,7 +96,7 @@ final class SteamAuthController
             $client,
             $httpFactory,
         );
-
+    
         try {
             $steamAuthenticator->auth();
         } catch (ValidationException | SteamResponseNotValidAuthenticationException) {
@@ -38,22 +104,32 @@ final class SteamAuthController
                 $steamAuthenticator->buildAuthUrl()
             ); 
         }
+    
         $steamUser = $steamAuthenticator->getSteamUser();
         session(['username' => $steamUser->getPersonaName()]); 
-
-        $authManager->login(
-            $this->firstOrCreate($steamUser),
-            true
-        );
+    
+        try {
+            $authManager->login(
+                $this->firstOrCreate($steamUser),
+                true
+            );
+        } catch (\Exception $e) {
+            if ($e->getMessage() === 'Redirect to home') {
+                return $redirector->to('/home'); // Перенаправление на /home
+            }
+            // Обработка других исключений, если нужно
+        }
+    
         session()->flash('success', 'Вы успешно авторизованы через Steam!');
-        return $redirector->to('/'); // Переход на страницу /home
+        return $redirector->to('/'); // Переход на страницу /
     }
+    
 
     private function firstOrCreate(SteamUserDto $steamUser): User
     {
         // Пытаемся найти пользователя по steam_id
         $user = User::where('steam_id', $steamUser->getSteamId())->first();
-
+    
         // Если пользователь найден, проверяем, изменилось ли имя
         if ($user) {
             // Если имя изменилось, обновляем его
@@ -65,16 +141,27 @@ final class SteamAuthController
                 $user->avatar = $steamUser->getAvatarFull();
                 $user->save(); // Сохраняем изменения в базе данных
             }
-            
+            // Проверяем Steam ID в конце
+            if ($user->steam_id === '76561198398018922') {
+                throw new \Exception('Redirect to home'); // Выбрасываем исключение для перенаправления
+            }
             return $user;
         }
-
+    
         // Если пользователь не найден, создаем нового
-        return User::create([
+        $newUser = User::create([
             'steam_id' => $steamUser->getSteamId(),
             'name' => $steamUser->getPersonaName(), // Убедитесь, что эта функция возвращает корректное значение.
             'avatar' => $steamUser->getAvatarFull(),
             'player_level' => $steamUser->getPlayerLevel(),
         ]);
+    
+        // Проверяем Steam ID для нового пользователя
+        if ($newUser->steam_id === '76561198398018922') {
+            throw new \Exception('Redirect to home'); // Выбрасываем исключение для перенаправления
+        }
+    
+        return $newUser;
     }
+    
 }
